49ce6f5bf4db0fa6f7c6dd414f35d5b08805e7bf
Joey M <172998670+mankovich@users.noreply.github.com>
2024-08-06 07:02:58 -0400

    pushing README and walkthrough video

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# ecommerce-back-end
+# Module 13 Challenge: E-Commerce Back End
+
+## Description of Application
+
+This application is the back-end for an ecommerce website, which employs Sequelize and working express.js API to interact with a PostgreSQL database of products. 
+
+## Walkthrough Video
+
+The video walkthrough may be viewed at this Google Drive [link.](https://drive.google.com/file/d/1WX0EJ1u7L4ffRSs_LfaSBBvCQyEMNkhp/view)
+
+The video may also be accessed in the application's [GitHub Repository.](https://github.com/mankovich/ecommerce-back-end)
+
+---
+&copy;2024 Mankovich, a person. All rights reserved. 
--- models/Product.js
+++ models/Product.js
@@ -2,6 +2,7 @@
 const { Model, DataTypes } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
+const { DECIMAL } = require('sequelize');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
@@ -22,10 +23,17 @@ Product.init(
     price: {
       type: DataTypes.DECIMAL,
       allowNull: false,
+      validate: {
+        isDecimal: true,
+      }
     },
     stock: {
       type: DataTypes.INTEGER,
       allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true,
+      }
     },
     category_id: {
       type: DataTypes.INTEGER,
